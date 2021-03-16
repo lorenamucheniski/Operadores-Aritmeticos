9aabcca9f858764999a87d64c785f9c085a4cae7
lorenamucheniski <lorenamucheniski26@gmail.com>
2021-03-16 20:01:31 -0300

    first commit

---
+++ src/main/java/OperadoresAritmeticos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lorena
+ */
+public class OperadoresAritmeticos {
+ 
+    public static void main(String[] args) {
+        
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual a " + m);
+        
+        
+        
+        int número = 5;
+        int valor = 5 + número++;
+        System.out.println(valor);
+        System.out.println(número);
+        
+       
+    }     
+
+}    
